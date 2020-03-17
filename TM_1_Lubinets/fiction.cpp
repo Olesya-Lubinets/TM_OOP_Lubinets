555ebd349c2414b6d6c2583845e9683d328a8719
Olesya_Lubinets <olesya.lubinec6@gmail.com>
2020-03-17 18:06:41 +0700

    Revert "Revert "new field: country""
    This reverts commit 3dae0c34e1a2eec135dcd77ecd6f8cb3b1a2d14b.

--- TM_1_Lubinets/fiction.cpp
+++ TM_1_Lubinets/fiction.cpp
@@ -3,6 +3,7 @@
 void fiction::InData(ifstream& ifst)
 {
     ifst >> director;
+    In_common(ifst);
 }
 
 void fiction::Out(ofstream& ofst) 
@@ -10,6 +11,7 @@ void fiction::Out(ofstream& ofst)
     ofst << "FICTION MOVIE "<<endl;
     ofst << "Director: "<< director << endl;
     ofst << "Title: "<< title << endl;
+    Out_common(ofst);
 }
 
 fiction::fiction()
